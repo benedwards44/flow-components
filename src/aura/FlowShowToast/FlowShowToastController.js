03f67de4da79f7ec07e1fc096be4919f79cd054b
Ben Edwards <ben@edwards.nz>
2022-09-15 14:47:25 +1200

    New variables for Toast

--- src/aura/FlowShowToast/FlowShowToastController.js
+++ src/aura/FlowShowToast/FlowShowToastController.js
@@ -5,9 +5,13 @@
         let title = component.get("v.title");
         let message = component.get("v.message");
         let type = component.get("v.type");
+        let mode = component.get("v.mode");
+        let duration = component.get("v.duration");
 
         var toastEvent = $A.get("e.force:showToast");
         toastEvent.setParams({
+            "mode" : mode,
+            "duration": duration,
             "title": title,
             "message": message,
             "type": type
